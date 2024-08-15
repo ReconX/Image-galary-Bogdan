0ca97c75e9f123b7af98e1aa5c13df05227a6c6e
ReconX <transformtosecure@gmail.com>
2024-08-15 11:40:12 -0700

    Building Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Cyber Security Architect 2" />
     </div>
   );
 }
